56c13ce5c6ffcb2332e8a27d53298fe772299ee3
Kyla Ward <126225912+ward0347@users.noreply.github.com>
2023-05-05 14:43:56 +0930

    adding new changes

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ A docassemble extension.
 ## Author
 
 Amber Whalland-Stedman, whal0035@flinders.edu.au
+Kyla Ward, ward0347@flinders.edu.au
 
 A change to keep GitHub happy
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012023S1LSC1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.whal0035LSCS1Draft\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nAmber Whalland-Stedman, whal0035@flinders.edu.au\r\n\r\nA change to keep GitHub happy',
+      long_description='# docassemble.whal0035LSCS1Draft\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nAmber Whalland-Stedman, whal0035@flinders.edu.au\r\nKyla Ward, ward0347@flinders.edu.au\r\n\r\nA change to keep GitHub happy',
       long_description_content_type='text/markdown',
       author='Amber Whalland-Stedman',
       author_email='whal0035@flinders.edu.au',
